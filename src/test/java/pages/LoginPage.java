bf5de463347ae1d17e6af1a70561ed6b4a10a003
kosmikapr229am <kosmikapr229am@gmail.com>
2022-10-18 20:15:27 +0530

    added LoginPage

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage {
+	
+	
+	@FindBy(xpath="//input[@name='username'")
+	WebElement username;
+
+}
